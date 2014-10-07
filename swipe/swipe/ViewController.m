703c6d4934fd9d4252a5b431c334331da602b2bb
jamintz <jasonamintz@gmail.com>
2014-10-06 21:27:41 -0400

    files

---
+++ swipe/swipe/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  swipe
+//
+//  Created by Jason Mintz on 10/6/14.
+//  Copyright (c) 2014 Jason Mintz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ swipe/swipe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  swipe
+//
+//  Created by Jason Mintz on 10/6/14.
+//  Copyright (c) 2014 Jason Mintz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
